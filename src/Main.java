9aae77ef342f919fd5041cc7d220618f1db3659b
fost <fostillo@gmail.com>
2024-07-10 20:23:58 -0500

    New code added

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 10; i++) {
-            System.out.println("B19 is great");
+            System.out.println("B19 super is great");
             Thread.sleep(2000);
         }
 
